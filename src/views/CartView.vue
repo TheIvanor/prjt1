fa9a957df7a6126450c923ff1220272acfcf1cd6
TheIvanor <ivan.portnyaga2@gmail.com>
2025-03-12 11:54:15 +0200

    prj1

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,30 +1,68 @@
 <template>
-    <div class="p-x-20">
-        <ul class="list bg-base-100 rounded-box shadow-md">
+  <div class="p-x-20">
+    <ul class="list bg-base-100 rounded-box shadow-md">
+      <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Список товорів у кошику</li>
 
-            <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Список товорів у кошику</li>
+      <li v-for="product in localProducts" :key="product.id" class="list-row">
+        <div>
+          <img
+            style="width: 100%; max-width: 150px"
+            :src="product.images[0]"
+            :alt="product.title"
+          />
+        </div>
+        <div>
+          <div>{{ product.title }}</div>
+          <div class="text-xs uppercase font-semibold opacity-60">{{ product.description }}</div>
+        </div>
+        <button class="btn btn-square" @click="countMinus(product)">
+          <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              opacity="0.5"
+              d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
+              stroke="#ffffff"
+              stroke-width="1.5"
+            />
+            <path d="M15 12H9" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" />
+          </svg>
+        </button>
+        <div class="font-bolt"><input type="number" :value="product.count" /></div>
 
-            <li v-for="product in localProducts" :key="product.id" class="list-row">
-                <div><img style="width: 100%;max-width: 150px;" :src="product.images[0]" :alt="product.title"/>
-                </div>
-                <div>
-                    <div>{{ product.title }}</div>
-                    <div class="text-xs uppercase font-semibold opacity-60">{{ product.description }}</div>
-                </div>
-                <div class="font-bolt ">{{ product.price }}</div>
-              </li>
-        </ul>
-          <div class="flex justify-end mt-5">
-              <button class="btn btn-primary">Оформити замовлення</button>
-          </div>
+        <button class="btn btn-square" @click="countPlus(product)">
+          <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M9 12H15"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M12 9L12 15"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z"
+              stroke="#ffffff"
+              stroke-width="2"
+            />
+          </svg>
+        </button>
+        <div class="font-bolt">{{ (product.price * product.count).toFixed(2) }}$</div>
+      </li>
+    </ul>
+    <div class="flex justify-end mt-5">
+      <button class="btn btn-primary">Оформити замовлення</button>
     </div>
+  </div>
 </template>
 
 <script setup>
-
-let localProducts = localStorage.getItem('cart');
-if (localProducts){
-  localProducts = JSON.parse(localProducts);
+let localProducts = localStorage.getItem('cart')
+if (localProducts) {
+  localProducts = JSON.parse(localProducts)
 }
-
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,99 +1,125 @@
 <script setup>
-import { ref, computed } from 'vue';
-let products = ref([]);
+import { ref, computed } from 'vue'
+let products = ref([])
 
-const cartProducts = ref([]);
+const cartProducts = ref([])
 
 const totoalPrise = computed(() => {
-return cartProducts.value.reduce((totoal, product) => totoal + product.price, 0)
-});
+  return cartProducts.value
+    .reduce((totoal, product) => totoal + product.price * product.count, 0)
+    .toFixed(2)
+})
 
-const addToCart = (product) =>{
-cartProducts.value.push(product);
-localStorage.setItem('cart', JSON.stringify(cartProducts.value));
-};
+const totoalCountProductsInCart = computed(() => {
+  return cartProducts.value.reduce((totoal, product) => totoal + product.count, 0)
+})
+
+const addToCart = (product) => {
+  const findIndex = cartProducts.value.findIndex((item) => item.id === product.id)
+
+  if (findIndex !== -1) {
+    cartProducts.value[findIndex].count += 1
+  } else {
+    cartProducts.value.push(product)
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cartProducts.value))
+}
 
 fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(data => {products.value = data.products});
+  .then((res) => res.json())
+  .then((data) => {
+    products.value = data.products.map((product) => {
+      product.count = 1
+      return product
+    })
+  })
 </script>
 
 <template>
   <dev>
     <div class="navbar bg-primary text-pimary-content shadow-sm">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none">
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span class="badge badge-sm indicator-item">{{ cartProducts.length }}</span>
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div class="flex-none">
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span class="badge badge-sm indicator-item">{{ totoalCountProductsInCart }}</span>
+            </div>
+          </div>
+          <div
+            tabindex="0"
+            class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+          >
+            <div class="card-body">
+              <span class="text-lg font-bold">{{ totoalCountProductsInCart }} товарів</span>
+              <span class="text-info">Сума: ${{ totoalPrise }}</span>
+              <div class="card-actions">
+                <a href="/cart" class="btn btn-primary btn-block">В корзину</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
         </div>
       </div>
-      <div
-        tabindex="0"
-        class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+    </div>
+
+    <div class="flex flex-wrap gap-4 mx-auto conteiner">
+      <div v-for="product in products" :key="product.id" class="card bg-base-100 w-80 shadow-xl">
+        <figure>
+          <img
+            style="width: 100%; max-width: 150px"
+            :src="product.images[0]"
+            :alt="product.title"
+          />
+        </figure>
         <div class="card-body">
-          <span class="text-lg font-bold">{{ cartProducts.length }} товарів</span>
-          <span class="text-info">Сума: ${{ totoalPrise }}</span>
-          <div class="card-actions">
-            <a href="/cart" class="btn btn-primary btn-block">В корзину</a>
+          <p class="text-center font-bold text-lg">{{ product.price }}</p>
+          <h2 class="card-title">{{ product.title }}</h2>
+          <p>{{ product.description }}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" @click="addToCart(product)">В корзину</button>
           </div>
         </div>
       </div>
     </div>
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabindex="0"
-        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a class="justify-between">
-            Profile
-            <span class="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-
-  <div class="flex flex-wrap gap-4 mx-auto conteiner">
-    <div v-for="product in products" :key="product.id" class="card bg-base-100 w-80 shadow-xl">
-  <figure>
-    <img style="width: 100%;max-width: 150px;" :src="product.images[0]" :alt="product.title"/>
-  </figure>
-  <div class="card-body">
-    <p class="text-center font-bold text-lg">{{ product.price }}</p>
-    <h2 class="card-title">{{ product.title }}</h2>
-    <p>{{ product.description }}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary" @click="addToCart(product)">В корзину</button>
-      </div>
-    </div>
-  </div>
-  </div>
-</dev>
-
+  </dev>
 </template>
