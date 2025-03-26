cd1b690bfa0b00e15974f151f29ae8b4164f4a68
TheIvanor <ivan.portnyaga2@gmail.com>
2025-03-26 12:01:34 +0200

    prj1

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="p-x-20">
+    <a href="/" class="btn btn-ghost text-xl">Головна</a>
     <ul class="list bg-base-100 rounded-box shadow-md">
       <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Список товорів у кошику</li>
 
@@ -54,8 +55,40 @@
         <div class="font-bolt">{{ (product.price * product.count).toFixed(2) }}$</div>
       </li>
     </ul>
+    <div class="flex flex-col gap-2">
+      <input v-model="userName" type="text" placeholder="Введіть ваше ім'я" class="input" />
+      <label class="input validator">
+        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+          <g fill="none">
+            <path
+              d="M7.25 11.5C6.83579 11.5 6.5 11.8358 6.5 12.25C6.5 12.6642 6.83579 13 7.25 13H8.75C9.16421 13 9.5 12.6642 9.5 12.25C9.5 11.8358 9.16421 11.5 8.75 11.5H7.25Z"
+              fill="currentColor"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M6 1C4.61929 1 3.5 2.11929 3.5 3.5V12.5C3.5 13.8807 4.61929 15 6 15H10C11.3807 15 12.5 13.8807 12.5 12.5V3.5C12.5 2.11929 11.3807 1 10 1H6ZM10 2.5H9.5V3C9.5 3.27614 9.27614 3.5 9 3.5H7C6.72386 3.5 6.5 3.27614 6.5 3V2.5H6C5.44771 2.5 5 2.94772 5 3.5V12.5C5 13.0523 5.44772 13.5 6 13.5H10C10.5523 13.5 11 13.0523 11 12.5V3.5C11 2.94772 10.5523 2.5 10 2.5Z"
+              fill="currentColor"
+            ></path>
+          </g>
+        </svg>
+        <input
+          v-model="userPhone"
+          type="tel"
+          class="tabular-nums"
+          required
+          placeholder="Ваш телефон"
+          pattern="[0-9]*"
+          minlength="10"
+          maxlength="10"
+          title="Must be 10 digits"
+        />
+      </label>
+      <p class="validator-hint">Must be 10 digits</p>
+    </div>
+
     <div class="flex justify-end mt-5">
-      <button class="btn btn-primary">Оформити замовлення</button>
+      <button class="btn btn-primary" @click="createOrder">Оформити замовлення</button>
     </div>
   </div>
 </template>
@@ -64,6 +97,9 @@
 import { ref } from 'vue'
 let localStorageCartProducts = ref([])
 let localProducts = localStorage.getItem('cart')
+let userName = ref('')
+let userPhone = ref('')
+
 if (localProducts) {
   localStorageCartProducts.value = JSON.parse(localProducts)
 }
@@ -75,5 +111,17 @@ function countPlus(product) {
 function countMinus(product) {
   if (product.count > 1) product.count -= 1
   localStorage.setItem('cart', JSON.stringify(localStorageCartProducts.value))
+}
+
+function createOrder() {
+  localStorage.setItem(
+    'order',
+    JSON.stringify({
+      name: userName.value,
+      phone: userPhone.value,
+      order: localStorageCartProducts.value,
+    }),
+  )
+  localStorage.removeItem('cart')
 }
 </script>
